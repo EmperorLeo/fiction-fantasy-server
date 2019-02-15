19633eace0751f7c77a08f3dc99d525bcdaf57f0
James Bruening <jwbruening@gmail.com>
2019-02-14 21:47:58 -0500

    persecuted to bool

--- FictionFantasyServer.Data/Entities/SpeciesEntity.cs
+++ FictionFantasyServer.Data/Entities/SpeciesEntity.cs
@@ -9,7 +9,7 @@ namespace FictionFantasyServer.Data.Entities
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Persecuted { get; set; }
+        public bool Persecuted { get; set; }
         public Stature Stature { get; set; }
         public DateTime Created { get; set; }
         public DateTime? Modified { get; set; }
